1366cabdbd5398f4a1bb4790824192ad7c039624
Sojan Varghese <sojanvarghese@Sojans-MacBook-Pro.local>
2025-07-10 11:36:11 +0530

    Implemented a robust local CSS copy approach

--- webhooks-frontend/src/index.js
+++ webhooks-frontend/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "../node_modules/@bigbinary/neetoui/dist/index.css"; // neetoUI styles
+// NeetoUI CSS imported locally due to React build restrictions
+// The CSS file is automatically copied from node_modules via scripts in package.json
+// This solves the issue where React cannot resolve @bigbinary/neetoui/dist/index.css
+import "./neetoui.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
